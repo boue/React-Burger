92680483c8d9b303f3521df2f7105c1b0913e44c
Germain Boue <boue.germain@gmail.com>
2018-03-11 17:53:12 -0700

    Fix typos in imports

--- react-burger/src/components/Burger/Burger.js
+++ react-burger/src/components/Burger/Burger.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
+import classes from './Burger.css';
+import BurgerIngredients from './BurgerIngredients/BurgerIngredients';
+
 const burger = (props) => {
   return (
     <div>
-      
+      <div className={classes.Burger}>
+        <BurgerIngredients type="bread-top" />
+        <BurgerIngredients type="cheese" />
+        <BurgerIngredients type="meat" />
+        <BurgerIngredients type="bread-bottom" />
+      </div>
     </div>
   );
 };
