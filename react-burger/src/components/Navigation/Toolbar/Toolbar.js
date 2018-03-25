67f08ebd0665a5aa77207a24cf7fec2154012c22
Germain Boue <boue.germain@gmail.com>
2018-03-24 18:43:01 -0700

    Customize logo

--- react-burger/src/components/Navigation/Toolbar/Toolbar.js
+++ react-burger/src/components/Navigation/Toolbar/Toolbar.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 //imports strings to be scoped to this component css modules
 import classes from './Toolbar.css';
+import Logo from '../../Logo/Logo';
 
 const toolbar = (props) => (
   <header className={classes.Toolbar}>
     <div>MENU</div>
-    <div>LOGO</div>
+    <Logo />
     <nav>...</nav>
   </header>
 );
